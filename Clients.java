611b348fc45e75f7e634ea40ea26eed07020cd62
Jose Reyes <josereyes@Joses-MacBook-Pro.local>
2021-02-11 13:45:53 -0600

    java classes

---
+++ Clients.java
@@ -0,0 +1,45 @@
+public class Clients {
+
+    private String user;
+    private String name;
+    private int cel;
+    private String mail;
+    private String store; 
+    private String password;
+    private int[] orders;
+
+    public String getUser ()
+    {
+        return this.user;
+    }
+
+    public String getName ()
+    {
+        return this.name;
+    }
+
+    public int getCel ()
+    {
+        return this.cel;
+    }
+
+    public String getMail ()
+    {
+        return this.mail;
+    }
+
+    public String getStore ()
+    {
+        return this.store;
+    }
+
+    public String getPassword ()
+    {
+        return this.password;
+    }
+
+    public int[] getOrders ()
+    {
+        return this.orders;
+    }
+}
---
+++ Coffee.java
@@ -0,0 +1,35 @@
+public class Coffee {
+    
+    private String kind;
+    private String sugar;
+    private String milk; 
+    private String extra;
+
+    public Coffee (String kind, String sugar, String milk, String extra)
+    {
+        this.kind = kind;
+        this.sugar = sugar;
+        this.milk = milk; 
+        this.extra = extra; 
+    }
+
+    public String getKind ()
+    {
+        return this.kind;
+    }
+
+    public String getSugar ()
+    {
+        return this.sugar;
+    }
+
+    public String getMilk ()
+    {
+        return this.milk;
+    }
+
+    public String getExtra ()
+    {
+        return this.extra;
+    }
+}
